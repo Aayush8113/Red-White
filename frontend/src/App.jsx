f0ff07438073efa30f43c89b7a01ca5fc318e1c7
Aayush Tripathi <aayushtripathi8113@gmail.com>
2026-05-01 17:20:36 +0530

    feat(ui): connect Shop page to routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Shop from './pages/Shop';
 import { AuthProvider } from './context/AuthContext';
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
-              <Route path="/shop" element={<div className="pt-32 text-center text-4xl">Shop Page Coming Soon</div>} />
+              <Route path="/shop" element={<Shop />} />
               <Route path="/cart" element={<div className="pt-32 text-center text-4xl">Cart Page Coming Soon</div>} />
               <Route path="/profile" element={<div className="pt-32 text-center text-4xl">Profile Page Coming Soon</div>} />
             </Routes>
