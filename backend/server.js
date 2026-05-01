1dec5c1ee2d77fae77910f26bc03557e2e4b8377
Aayush Tripathi <aayushtripathi8113@gmail.com>
2026-05-01 17:15:23 +0530

    feat(ui): connect book routes to server

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/books', require('./routes/bookRoutes'));
 
 app.get('/', (req, res) => {
     res.send('API is running...');
