a7e846eb8aa75ee300469496d99f8cca2e236e6c
Aayush Tripathi <aayushtripathi8113@gmail.com>
2026-05-03 18:39:19 +0530

    feat(routes): finalize application routing and global layout styling

--- NodeJS/Blog Store/client/src/App.jsx
+++ NodeJS/Blog Store/client/src/App.jsx
@@ -4,17 +4,23 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import CreateBlog from './pages/CreateBlog';
+import BlogDetail from './pages/BlogDetail';
+import Dashboard from './pages/Dashboard'; 
+import EditBlog from './pages/EditBlog';   
 
 function App() {
   return (
     <Router>
       <Navbar /> 
-      <div style={{ minHeight: '80vh' }}>
+      <div style={{ minHeight: '80vh', backgroundColor: '#fafafa', paddingBottom: '40px' }}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/create" element={<CreateBlog />} />
+          <Route path="/blog/:id" element={<BlogDetail />} />
+          <Route path="/dashboard" element={<Dashboard />} /> 
+          <Route path="/edit/:id" element={<EditBlog />} />    
         </Routes>
       </div>
     </Router>
