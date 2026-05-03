d54591bc368e55062fab2589681aeba3e66f524a
Aayush Tripathi <aayushtripathi8113@gmail.com>
2026-05-03 18:52:25 +0530

    feat(server): integrate upload routing and static file serving

--- NodeJS/Blog Store/server/server.js
+++ NodeJS/Blog Store/server/server.js
@@ -1,28 +1,27 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db.js');
 
-
 dotenv.config();
-
-
 connectDB();
 
 const app = express();
 
-
 app.use(cors()); 
 app.use(express.json()); 
 
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/blogs', require('./routes/blogRoutes'));
+app.use('/api/comments', require('./routes/commentRoutes'));
+app.use('/api/upload', require('./routes/uploadRoutes'));
+
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.get('/', (req, res) => {
   res.send('Blog API is running...');
 });
-
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/blogs', require('./routes/blogRoutes'));
-app.use('/api/comments', require('./routes/commentRoutes')); 
 
 const PORT = process.env.PORT || 5000;
 
