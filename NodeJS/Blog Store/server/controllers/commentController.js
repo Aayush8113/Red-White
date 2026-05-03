386e8cf766b92570a48288fb34c64811041b1baf
Aayush Tripathi <aayushtripathi8113@gmail.com>
2026-05-03 20:39:08 +0530

    feat(api): trigger real-time notifications for blog comments

--- NodeJS/Blog Store/server/controllers/commentController.js
+++ NodeJS/Blog Store/server/controllers/commentController.js
@@ -1,20 +1,30 @@
 const Comment = require('../models/Comment');
 const Blog = require('../models/Blog');
+const Notification = require('../models/Notification');
 
 const addComment = async (req, res) => {
   try {
     const { text } = req.body;
     
     const blog = await Blog.findById(req.params.blogId);
-    if (!blog) {
-      return res.status(404).json({ message: 'Blog not found' });
-    }
+    if (!blog) return res.status(404).json({ message: 'Blog not found' });
 
     const comment = await Comment.create({
       text,
       blogId: req.params.blogId,
       user: req.user.id
     });
+
+
+    if (blog.author.toString() !== req.user.id) {
+      await Notification.create({
+        recipient: blog.author,
+        sender: req.user.id,
+        blogId: blog._id,
+        type: 'comment',
+        message: 'commented on your post'
+      });
+    }
 
     const populatedComment = await comment.populate('user', 'name');
     res.status(201).json(populatedComment);
@@ -25,10 +35,7 @@ const addComment = async (req, res) => {
 
 const getComments = async (req, res) => {
   try {
-    const comments = await Comment.find({ blogId: req.params.blogId })
-      .populate('user', 'name')
-      .sort({ createdAt: -1 }); 
-      
+    const comments = await Comment.find({ blogId: req.params.blogId }).populate('user', 'name').sort({ createdAt: -1 });
     res.status(200).json(comments);
   } catch (error) {
     res.status(500).json({ message: 'Server Error', error: error.message });
