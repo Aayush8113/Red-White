6716d45e04b216743b0451fbc45ed62ee007814c
Aayush Tripathi <aayushtripathi8113@gmail.com>
2026-05-03 18:35:28 +0530

    feat(services): complete CRUD operations for blog management

--- NodeJS/Blog Store/client/src/api/blogService.js
+++ NodeJS/Blog Store/client/src/api/blogService.js
@@ -1,7 +1,13 @@
 import API from './axiosConfig';
 
 export const fetchBlogs = async (category = '') => {
-  const response = await API.get(`/blogs${category ? `?category=${category}` : ''}`);
+  const url = category ? `/blogs?category=${category}` : '/blogs';
+  const response = await API.get(url);
+  return response.data;
+};
+
+export const fetchBlogById = async (id) => {
+  const response = await API.get(`/blogs/${id}`);
   return response.data;
 };
 
@@ -17,5 +23,11 @@ export const updateBlog = async (id, blogData) => {
 
 export const deleteBlog = async (id) => {
   const response = await API.delete(`/blogs/${id}`);
+  return response.data;
+};
+
+
+export const likeBlog = async (id) => {
+  const response = await API.put(`/blogs/${id}/like`);
   return response.data;
 };
