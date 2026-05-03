f0f668b6f337e4f0af33a12467686ea2315ef98d
Aayush Tripathi <aayushtripathi8113@gmail.com>
2026-05-03 18:50:47 +0530

    feat(models): expand Blog schema with media support and indexing

--- NodeJS/Blog Store/server/models/Blog.js
+++ NodeJS/Blog Store/server/models/Blog.js
@@ -4,8 +4,9 @@ const blogSchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
   category: { type: String, required: true, index: true },
+  coverImage: { type: String, default: '' },
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }] // Array of user IDs who liked
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }] 
 }, { timestamps: true });
 
 module.exports = mongoose.model('Blog', blogSchema);
