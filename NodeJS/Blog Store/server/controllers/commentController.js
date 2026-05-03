33fa6f1123c4849b942db9dd48a65adf64ed3cee
Aayush Tripathi <aayushtripathi8113@gmail.com>
2026-05-03 18:12:24 +0530

    feat(controllers): implement comment creation and retrieval logic

---
+++ NodeJS/Blog Store/server/controllers/commentController.js
@@ -0,0 +1,38 @@
+const Comment = require('../models/Comment');
+const Blog = require('../models/Blog');
+
+const addComment = async (req, res) => {
+  try {
+    const { text } = req.body;
+    
+    const blog = await Blog.findById(req.params.blogId);
+    if (!blog) {
+      return res.status(404).json({ message: 'Blog not found' });
+    }
+
+    const comment = await Comment.create({
+      text,
+      blogId: req.params.blogId,
+      user: req.user.id
+    });
+
+    const populatedComment = await comment.populate('user', 'name');
+    res.status(201).json(populatedComment);
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error', error: error.message });
+  }
+};
+
+const getComments = async (req, res) => {
+  try {
+    const comments = await Comment.find({ blogId: req.params.blogId })
+      .populate('user', 'name')
+      .sort({ createdAt: -1 }); 
+      
+    res.status(200).json(comments);
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error', error: error.message });
+  }
+};
+
+module.exports = { addComment, getComments };
