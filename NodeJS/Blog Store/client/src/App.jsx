8dcc6ac9db55b2f8ed4578f693e007f04b10105f
Aayush Tripathi <aayushtripathi8113@gmail.com>
2026-05-03 18:32:01 +0530

    feat(routes): register CreateBlog route and finalize navigation mapping

--- NodeJS/Blog Store/client/src/App.jsx
+++ NodeJS/Blog Store/client/src/App.jsx
@@ -3,16 +3,18 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import CreateBlog from './pages/CreateBlog';
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      <Navbar /> 
       <div style={{ minHeight: '80vh' }}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/create" element={<CreateBlog />} />
         </Routes>
       </div>
     </Router>
