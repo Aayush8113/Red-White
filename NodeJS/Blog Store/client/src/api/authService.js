5f325439034f85161b4f2db13e18f351773e22e7
Aayush Tripathi <aayushtripathi8113@gmail.com>
2026-05-03 19:04:01 +0530

    feat(services): implement profile update utility and local storage synchronization

--- NodeJS/Blog Store/client/src/api/authService.js
+++ NodeJS/Blog Store/client/src/api/authService.js
@@ -19,3 +19,11 @@ export const login = async (userData) => {
 export const logout = () => {
   localStorage.removeItem('userInfo');
 };
+
+export const updateProfile = async (userData) => {
+  const response = await API.put('/users/profile', userData);
+  const existingUser = JSON.parse(localStorage.getItem('userInfo'));
+  const updatedStorage = { ...existingUser, ...response.data };
+  localStorage.setItem('userInfo', JSON.stringify(updatedStorage));
+  return response.data;
+};
